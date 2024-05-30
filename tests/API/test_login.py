938732f0d0561e637b4c109c321fa3a617475495
Max Rusin <maxunris@gmail.com>
2024-05-30 16:40:30 +0300

    Добавление проверок

--- tests/API/test_login.py
+++ tests/API/test_login.py
@@ -1,11 +1,13 @@
-from utils.http_methods import HttpMethods
+import json
+
 from utils.api import LitresAPI
-import os
+from utils.cheking import Checking
 
 """Успешный вход"""
 def test_successful_login():
     result_post = LitresAPI.post_login()
-    print(result_post)
-
-
-
+    Checking.check_status_code(result_post, 200)
+    # token = json.loads(result_post.text)
+    # print(list(token))
+    Checking.check_json_toket(result_post, ['status', 'error', 'payload'])
+    Checking.check_json_value(result_post, 'error', None)
---
+++ utils/cheking.py
@@ -0,0 +1,27 @@
+"""Методы для проверки ответов запросов"""
+import json
+
+
+class Checking():
+    """Метод для проверки статус-кода"""
+    @staticmethod
+    def check_status_code(result, status_code):
+        assert status_code == result.status_code
+        print("Статус код - правильный")
+
+    """Метод для проверки наличия обязательных полей в ответе запроса"""
+
+    @staticmethod
+    def check_json_toket(result, expected_value):
+        token = json.loads(result.text)
+        assert list(token) == expected_value
+        print("Обязательные поля - корректные")
+
+
+    """Метод для проверки обязательных полей"""
+    @staticmethod
+    def check_json_value(result, field_name, expected_value):
+        check = result.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value
+        print("Значение поля " + field_name + " верное!")

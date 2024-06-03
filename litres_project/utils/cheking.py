021712312061c589f03c15cb68076bcd995665be
Max Rusin <maxunris@gmail.com>
2024-06-04 00:59:08 +0300

    Убраны комментарии

--- litres_project/utils/cheking.py
+++ litres_project/utils/cheking.py
@@ -1,15 +1,12 @@
-"""Методы для проверки ответов запросов"""
 import json
 
 
 class Checking():
-    """Метод для проверки статус-кода"""
     @staticmethod
     def check_status_code(result, status_code):
         assert status_code == result.status_code
         print("Статус код - правильный")
 
-    """Метод для проверки наличия обязательных полей в ответе запроса"""
 
     @staticmethod
     def check_json_token(result, expected_value):
@@ -18,7 +15,6 @@ class Checking():
         print("Обязательные поля - корректные")
 
 
-    """Метод для проверки обязательных полей"""
     @staticmethod
     def check_json_value(result, field_name, expected_value):
         check = result.json()
